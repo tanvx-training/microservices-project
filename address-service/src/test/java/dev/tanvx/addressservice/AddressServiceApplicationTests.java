e6de85f408209704ac7264ad1d4da8c6b1965ae2
TanVX <tanvx.training@gmail.com>
2024-12-20 01:37:03 +0700

    first commit

---
+++ address-service/src/test/java/dev/tanvx/addressservice/AddressServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tanvx.addressservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
